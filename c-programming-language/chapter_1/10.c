1a14a7d9fa379e9aa5fd9445d49962047e416f61
frtavonatti <frtavonatti@gmail.com>
2025-06-09 16:48:41 +0100

    Starting exercises 9-10

---
+++ c-programming-language/chapter_1/10.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+// Exercise 1.10: Copy input to its output replacing eah tab by \t, each backspace by \b and each backslash by \\.
+main() 
+{
+
+}
---
+++ c-programming-language/chapter_1/9.c
@@ -0,0 +1,7 @@
+#include <stdio.h> 
+
+// Exercise 1.9: Copy input to its output replacing ech string of one or more blanks by a single blank
+main()
+{ 
+  c = getchar()
+}
