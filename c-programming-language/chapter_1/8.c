660ff63e20562b4f8c686538373d50f6ac166ebc
frtavonatti <frtavonatti@gmail.com>
2025-06-11 00:01:33 +0100

    Refactor Exercise 1.8: Improve counting of blanks, tabs, and newlines

--- c-programming-language/chapter_1/8.c
+++ c-programming-language/chapter_1/8.c
@@ -1,24 +1,20 @@
 #include <stdio.h>
 
 // Exercise 1.8: Count blanks, tab and newlines
-
-// [ ] Pending review
-// Escape sequences: \n = newline || \t = tab || \b = backspace || \ = double quote || \\ backslash itself  
 int main() 
 {
   int blank = 0, tab = 0, newline = 0;
+  int c; 
 
-  int c = getchar(); 
-  for (int i = 0; i < c; i++) {
-    if (c = '\n') {
-      newline++;
-    } else if (c = '\t') {
-      tab++;
-    } else if (c = ' ') {
-      blank++;
+  // Use ctrl + D to end the execution of the program (EOF)
+  while ((c = getchar()) != EOF) {
+    if (c == '\n') {
+      ++newline;
+    } else if (c == '\t') {
+      ++tab;
+    } else if (c == ' ') {
+      ++blank;
     }
-
-    getchar();
   }
-  printf("%3.0d, %3.0d, %3.0d", blank, tab, newline);
+  printf("Blanks: %d, Tabs: %d, Newlines: %d", blank, tab, newline);
 }
--- c-programming-language/chapter_1/9.c
+++ c-programming-language/chapter_1/9.c
@@ -1,7 +1,20 @@
 #include <stdio.h> 
 
 // Exercise 1.9: Copy input to its output replacing ech string of one or more blanks by a single blank
-main()
-{ 
-  c = getchar()
+int main() 
+{
+  int blank = 0, tab = 0, newline = 0;
+  int c; 
+
+  // Use ctrl + D to end the execution of the program (EOF)
+  while ((c = getchar()) != EOF) {
+    if (c == '\n') {
+      ++newline;
+    } else if (c == '\t') {
+      ++tab;
+    } else if (c == ' ') {
+      ++blank;
+    }
+  }
+  printf("Blanks: %d, Tabs: %d, Newlines: %d", blank, tab, newline);
 }
