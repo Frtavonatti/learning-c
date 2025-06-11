cc2e8e9a452a2fbb3411cd4c1ecc95b6ab730089
frtavonatti <frtavonatti@gmail.com>
2025-06-11 10:33:21 +0100

    Completed exercise 9

--- c-programming-language/chapter_1/9.c
+++ c-programming-language/chapter_1/9.c
@@ -1,20 +1,26 @@
 #include <stdio.h> 
 
+// [ ] Pending Review
+
 // Exercise 1.9: Copy input to its output replacing ech string of one or more blanks by a single blank
 int main() 
 {
-  int blank = 0, tab = 0, newline = 0;
-  int c; 
+  int c;
+  int prev_blank = 0; 
 
-  // Use ctrl + D to end the execution of the program (EOF)
+  // Use ctrl + D (linux) / ctrl + Z (windows) to end the execution of the program (EOF)
   while ((c = getchar()) != EOF) {
-    if (c == '\n') {
-      ++newline;
-    } else if (c == '\t') {
-      ++tab;
-    } else if (c == ' ') {
-      ++blank;
+    if (c == ' ') {
+      if (!prev_blank) {
+        putchar(' ');
+        prev_blank = 1;
+      }
+      // If the next char is also a blank, skip printing
+    } else {
+      prev_blank = 0;
+      putchar(c);
     }
   }
-  printf("Blanks: %d, Tabs: %d, Newlines: %d", blank, tab, newline);
+  
+  return 0;
 }
