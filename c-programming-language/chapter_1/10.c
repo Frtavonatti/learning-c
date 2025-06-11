e9809c7c9afbeecb36968744387a01e1bdb7f7e6
frtavonatti <frtavonatti@gmail.com>
2025-06-11 10:51:59 +0100

    Completed exercise 10

--- c-programming-language/chapter_1/10.c
+++ c-programming-language/chapter_1/10.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
 
-// Exercise 1.10: Copy input to its output replacing eah tab by \t, each backspace by \b and each backslash by \\.
-main() 
-{
+// [ ] Pendieng Review
 
+// Exercise 1.10: Copy input to its output replacing each tab by \t, each backspace by \b and each backslash by \\.
+int main() 
+{
+  int c;
+
+  while ((c = getchar() != EOF)) {
+    if (c == '\t') {
+      putchar('\\');
+      putchar('\t');
+    } else if (c == '\b') {
+      putchar('\\');
+      putchar('\b');
+    } else if (c == '\\') {
+      putchar('\\');
+      putchar('\\');
+    } else {
+      putchar(c);
+    }
+  }
+
+  return 0;
 }
