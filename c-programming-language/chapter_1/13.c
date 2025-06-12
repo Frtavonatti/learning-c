399881428431d34ad149e335fe5b146a0a119a20
frtavonatti <frtavonatti@gmail.com>
2025-06-12 16:32:54 +0100

    On process: exercises 13-14

---
+++ c-programming-language/chapter_1/13.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+// [ ] Pendieng Review
+
+// Exercise 13: Write program to print a histogram of the lenghts of words in its input (horizontal and vertical). 
+int main () {
+  int lengtharr[10];
+  int c, wlength, otherlength;
+
+  wlength = otherlength = 0;
+
+  for (int i = 0; i < 9; ++i) {
+    lengtharr[i] = 0;
+  }
+
+  while ((c = getchar()) != EOF) {
+    if (c == ' ' || c == '\n' || c == '\t') {
+      if (lengtharr[wlength])
+        ++lengtharr[wlength];
+      else
+        ++otherlength;
+      wlength = 0;
+    } else {
+      ++wlength;
+    }
+  }
+
+  printf("Length histogram\n");
+  for (int i = 0; i < 9; ++i) {
+    for (int j = lengtharr[i]; j > 0; --j)
+      printf("#");
+    printf("\n");
+  }
+
+  printf(", Otherlength: %d", otherlength);
+  return 0;
+}
---
+++ c-programming-language/chapter_1/test.py
@@ -0,0 +1,31 @@
+import sys
+
+wlength = 0
+anotherlength = 0
+lengtharr = [0 for i in range(9)]
+
+# Read input as a string
+data = sys.stdin.read()
+
+for c in data:
+    if c == ' ' or c == '\n' or c == '\t':
+        if wlength > 0:
+            if wlength < len(lengtharr):
+                lengtharr[wlength] += 1
+            else:
+                anotherlength += 1
+            wlength = 0
+    else:
+        wlength += 1
+
+# Count the last word even if it doesnt end with a separator
+if wlength > 0:
+    if wlength < len(lengtharr):
+        lengtharr[wlength] += 1
+    else:
+        anotherlength += 1
+
+for i, val in enumerate(lengtharr):
+    print(f"{i}: {'#' * val}")
+if anotherlength > 0:
+    print(f"Words with length >= {len(lengtharr)}: {anotherlength}")
