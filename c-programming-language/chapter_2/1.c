3130471fe6f2aaea545bd680b5de8268430ace1a
frtavonatti <frtavonatti@gmail.com>
2025-06-19 16:55:21 +0100

    Ongoing exercises 2.1 - 2.2

--- c-programming-language/chapter_2/1.c
+++ c-programming-language/chapter_2/1.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
-#include <limits.h>
-#include <float.h>
 
 // - [ ] PENDING REVIEW 
 // Exercise 2.1: Write a program to determine the ranges of char, short, int and long variables, both signed and unsigned
 
-int main() {
-  unsigned char example = 0;
+#define FIND_UNSIGNED_MAX(type)           \
+    do {                                  \
+        type x = 0, prev = 0;             \
+        while (++x != 0) prev = x;        \
+        printf("Max for " #type ": %u\n", prev); \
+    } while(0)
 
-  return 0;
+int main() {
+    FIND_UNSIGNED_MAX(unsigned char);   // 255
+    FIND_UNSIGNED_MAX(unsigned short);  // 65535
+    FIND_UNSIGNED_MAX(unsigned int);    // 4294967295
+    FIND_UNSIGNED_MAX(unsigned long);   // 
+    return 0;
 }
