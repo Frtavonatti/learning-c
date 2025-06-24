e27c02438f29c44f8f7813cbce7394f49e48f04d
frtavonatti <frtavonatti@gmail.com>
2025-06-24 21:09:40 +0100

    Reviewed exercise 1.18

--- c-programming-language/chapter_1/18.c
+++ c-programming-language/chapter_1/18.c
@@ -1,39 +1,34 @@
 #include <stdio.h>
+#include <stdbool.h>
 #define MAXLINES   1000
-#define MAXARRAYS  10
 
-int get_line(char s[], int lim)
-int str_len(char arr[]);
-int arr_len(char arr[][MAXARRAYS]);
+int get_line(char s[], int lim);
+int str_len(char str[]);
 
-// PENDING REVIEW
 // Exercise 1.18: Write a program to remove trailing blanks and tabs from each line of input and to delete entirely blank lines 
 int main(){
-  int c;
-  int len, array_len, last;
-  char store_arr[MAXLINES][MAXARRAYS];
-  char new_arr[MAXLINES][MAXARRAYS];
+  int len;
+  char store_arr[MAXLINES];
+  char new_arr[MAXLINES];
 
-  // FIX: c is never inicialized inside the loop
-  for (int i=0; i < MAXARRAYS && c == '\n'; ++i) 
-    get_line(store_arr[i], MAXARRAYS-1);
+  len = get_line(store_arr, MAXLINES);
 
-  array_len = arr_len(store_arr);
-
-  /* copy excluding trailing blank spaces and tabs */
-  for (int i=0; i<array_len; ++i) {
-    len = str_len(store_arr[i]);
-    for (int last = len-1; last >= 0; --last) {
-      if (store_arr[i][last] != ' ' && store_arr[i][last] != '\t')
-        new_arr[i][last] = store_arr[i][last];
-    }
+  int end = len - 1;
+  while (end >= 0 && (store_arr[end] == ' ' || store_arr[end] == '\t' || store_arr[end] == '\n')) {
+    end--;
   }
 
-  for(int i=0; i<array_len; ++i) {
-    len = str_len(new_arr[i]);
-    for (int j=0; j<len; ++j) 
-      printf("%c", new_arr[i][j]);
+  if (end >= 0) {
+    for (int i = 0; i <= end; i++) {
+      new_arr[i] = store_arr[i];
+    } 
+    new_arr[end + 1] = '\0';
   }
+
+  printf("Copied input into array: %s", store_arr);
+  printf("New array without blanks: %s\n", new_arr);
+  printf("Original string length: %d\n", len);
+  printf("New string length: %d\n", str_len(new_arr));
   return 0;
 }
 
@@ -47,16 +42,10 @@ int get_line(char s[], int lim) {
   return i;
 }
 
-int str_len(char arr[]) {
+int str_len(char str[]) {
   int i = 0;
-  while (arr[i] != '\0')
+  while (str[i] != '\0') {
     ++i;
-  return i;
-}
-
-int arr_len(char arr[][MAXARRAYS]) {
-  int i = 0;
-  while (arr[i][0] != '\0' && i < MAXLINES)
-    ++i;
+  }
   return i;
 }
