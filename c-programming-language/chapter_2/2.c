10d26839cdba9de0ece35045eafe5b944ad891f6
frtavonatti <frtavonatti@gmail.com>
2025-11-30 16:41:39 +0000

    Refactor exercise 2.2

--- c-programming-language/chapter_2/2.c
+++ c-programming-language/chapter_2/2.c
@@ -1,68 +1,52 @@
 #include <stdio.h> 
 
-enum { lim = 40 };
+enum { MAXLINE = 40 };
 
-int example () {
+void getline_with_logical_ops() {
   int c;
-  char s[lim] = {0};
+  char s[MAXLINE] = {0}; // Initializes the array with 0s
   int i;
 
-  for (i = 0; i < lim-2 && (c=getchar()) != '\n' && c != EOF; ++i) 
+  for (i = 0; i < MAXLINE-2 && (c=getchar()) != '\n' && c != EOF; ++i) 
     s[i] = c;
 
-  s[i++] = '\n';
+  if (c == '\n' && i < MAXLINE - 1)
+    s[i++] = '\n';
   s[i] = '\0'; // Null-terminate the string
 
-  for (int j = 0; j < i; ++j)
-    printf("%c", s[j]);
-  
-  return 0;
+  printf("Result: %s\n", s);
 }
 
 // Exercise 2.2: Write a loop equivalent to the for loop above without using && or ||
-int solution () {
+void getline_without_logical_ops() {
   int c;
-  char s[lim] = {0};
+  char s[MAXLINE] = {0};
   int i;
 
   for (i = 0; ; ++i) {
-    if (i >= lim-1) 
+    if (i >= MAXLINE-2) 
       break;
-    if ((c=getchar()) == '\n')
-      break;
+
+    c = getchar();
     if (c == EOF)
+      break;
+    if (c == '\n')
       break;
     s[i] = c;
   }
 
-  s[i++] = '\n';
+  if (c == '\n' && i < MAXLINE - 1)
+    s[i++] = '\n';
   s[i] = '\0';
 
-  for (int j = 0; j < i; ++j)
-    printf("%c", s[j]);
-  
-  return 0;
+  printf("Result: %s", s);
 }
 
 int main () {
-  example();
-  solution();
+  printf("Test 1 - with logical operators:\n");
+  getline_with_logical_ops();
+
+  printf("Test 2 - without logical operators:\n");
+  getline_without_logical_ops();
   return 0;
 }
-
-// int incorrect_solution () {
-//   int c;
-//   char s[lim];
-//   int i;
-
-//   for (i = 0; i < lim-1 == ((c=getchar()) != '\n') == (c != EOF); ++i) 
-//     s[i] = c;
-  
-//   s[i++] = '\n';
-//   s[i] = '\0';
-
-//   for (int j = 0; j < i; ++j)
-//     printf("%c", s[j]);
-  
-//   return 0;
-// }
